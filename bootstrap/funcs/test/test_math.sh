163e6fe11dc0d3d0002b9af27f468f7b1f5f0d38
Daniel Baker <daniel.n.baker@gmail.com>
2022-03-17 13:01:17 -0700

    Added a main setup script, a partitioning script, several helper function scripts, and documented the learning process in markdown files.
    cleaned up the math functions and finished writing the test.
    
    added documentation to the math functions.
    
    made test script executable.
    
    initial commit of disk.sh helper function script.
    
    added a function to return only disk devices with their symbolic link.
    
    made a function to show disks to the user.
    
    added a function to select a disk from the list by its index.
    
    added a function to query the user for a disk number.
    
    added a function that shows users all the disks and allows them to add them to the pool.
    
    removing old prototyping functions.
    
    added a function to query the user to select disks for the zpool and verifies that it is correct.
    
    added documentation to the functions in disk.sh.
    
    adding the setup and partition helper function script.
    
    changed the dir variable name to be more accurate.
    
    corrected the disk.sh script imports so it can be sourced from another script. imported it into the setup.sh script to use the new disk selection functions.
    
    removed the executable flag from both scripts. changed the call of a function so that the assigned variable becomes an array. was not using arrays correctly. changed the printing of the list of selected disks so they are each on their own line.
    
    initial commit of partition script.
    
    added a function to get the system memory as a float so it can be rounded up for the partition size.
    
    fixed documentation.
    
    initial commit of questions file.
    
    Added some knowledge about what `mount -o bind` does.
    
    removed variable from top level setup.sh script that was relevant before using functions to define the disks.
    
    initial commit of partition and dataset layout blog post.
    
    added root pool and encrypted system dataset to partition script.
    
    Added a question about how datasets with canmount=off work when a child has it on.
    
    added system partitioning to the partition script.
    
    added esp formatting and mounting
    
    added creating optional user datasets to partition script.

---
+++ bootstrap/funcs/test/test_math.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+this_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$this_dir/../math.sh"
+
+
+function test_func_with_arrays () {
+    local funcname=$1
+    local -n domains=$2
+    local -n truths=$3
+
+    declare -i COUNT=0
+    for idx in "${!domains[@]}"; do
+        local actual=$($1 "${domains[idx]}")
+        local truth="${truths[idx]}"
+
+        if [[ $(echo "$actual" != "$truth" | bc) -ne 0 ]]; then
+            ((COUNT++))
+            echo "$funcname: $actual not equal to $truth at index $idx"
+        fi
+    done
+
+    if [[ $COUNT == 0 ]]; then
+        echo "$funcname: passed with no failures"
+    fi
+}
+
+
+function test_log10 () {
+    declare -a ARRAY=(1 10 100 1000)
+    declare -a TRUTH=(0  1   2    3)
+    test_func_with_arrays log10 ARRAY TRUTH
+}
+test_log10
+
+
+function test_floor () {
+    declare -a ARRAY=(0 0.0 0.1 0.49 0.5 0.51 0.9 1 1.0 1.1 1.49 1.5 1.51 1.99 2.0 2.4 2.5)
+    declare -a TRUTH=(0   0   0    0   0    0   0 1   1   1    1   1    1    1   2   2   2)
+    test_func_with_arrays floor ARRAY TRUTH
+}
+test_floor
+
+
+function test_ceil () {
+    declare -a ARRAY=(0 0.0 0.1 0.49 0.5 0.51 0.9 1 1.0 1.1 1.49 1.5 1.51 1.99 2.0 2.4 2.5)
+    declare -a TRUTH=(0   0   1    1   1    1   1 1   1   2    2   2    2    2   2   3   3)
+    test_func_with_arrays ceil ARRAY TRUTH
+}
+test_ceil
+
+
+function test_num_digits () {
+    declare -a ARRAY=(1 5 9 10 15 99 100 501 999 1000)
+    declare -a TRUTH=(1 1 1  2  2  2   3   3   3    4)
+    test_func_with_arrays num_digits ARRAY TRUTH
+}
+test_num_digits
