582f9d841c38f56f62759606fada56733408ece6
Daniel Baker <daniel.n.baker@gmail.com>
2022-03-18 21:45:01 -0700

    added documentation to the functions in disk.sh.

--- bootstrap/funcs/disk.sh
+++ bootstrap/funcs/disk.sh
@@ -3,28 +3,39 @@
 source ./math.sh
 
 
+# Returns a newline seperate string of all the disks devices by ID
+# along with dereference if it is a symbolic link. Will also return
+# partitions and other items.
 function disks_by_id_symbolic () {
     echo "$(stat --format="%N" /dev/disk/by-id/*)"
 }
 
 
+# Takes a line from `disks_by_id_symbolic` and returns the internal
+# kernel device name part of the string.
 function disk_by_id_kname () {
     disk=$1
     echo $disk | grep -oP "(?<='../../).*?(?=')"
 }
 
 
+# Takes a line from `disks_by_id_symbolic` and returns the symbolic link
+# path.
 function disk_by_id_sympath () {
     disk=$1
     echo $disk | grep -oP "(?<=').*?(?=' ->)"
 }
 
 
+# Returns a newline seperated string all the block devices showing only
+# the interal kernel device name and type.
 function lsblk_filesystems () (
     echo "$(lsblk -o kname,type)"
 )
 
 
+# Takes the output from `lsblk_filesystems` and returns an array of the
+# internal kernel device names that are disk types.
 function filter_lsblk_by_disk () {
     declare devices=$1
     declare -a filtered=()
@@ -43,6 +54,9 @@ function filter_lsblk_by_disk () {
 }
 
 
+# Takes the output from `disks_by_id_symbolic` and returns only the
+# devices that are disks. Uses the output of `filter_lsblk_by_disk` to
+# know which devices are disks.
 function get_only_disks () {
     local all_disks=$(disks_by_id_symbolic)
 
@@ -59,6 +73,8 @@ function get_only_disks () {
 }
 
 
+# Shows all the disks on the system with a number next to them. Used
+# with another function to select disks from the list.
 function show_only_disks () {
     local only_disks=$(get_only_disks | column -t)
     local num_disks=$(get_only_disks | wc -l)
@@ -72,6 +88,8 @@ function show_only_disks () {
 }
 
 
+# Given an input index, will return the associated disk as shown by
+# `show_only_disks`. The return will be the disk ID symbolic path.
 function select_only_disks_by_idx () {
     local idx=$1
 
@@ -87,6 +105,8 @@ function select_only_disks_by_idx () {
 }
 
 
+# Asks the user to select a disk shown by `show_only_disks`. Will check
+# for bad inputs. Will alos return if 0 or nothing is given.
 function ask_for_disk_by_idx () {
     while true; do
         read -p "Select a disk by number. Return 0 or nothing to finish: " user_idx
@@ -119,6 +139,9 @@ function ask_for_disk_by_idx () {
 }
 
 
+# Shows the user a list of disks to add to the pool and allows them
+# to select multiple disks. Exits when the user returns 0 or nothing.
+# Returns an array of the disk device paths.
 function add_disks_to_pool () {
     show_only_disks
 
@@ -141,6 +164,9 @@ function add_disks_to_pool () {
 }
 
 
+# Asks the user to select disks for the pool and verifies that selected
+# disks are good before returning the array of disks. The user can try
+# again if there is a mistake.
 function ask_user_for_disks () {
     while true; do
         local disks=$(add_disks_to_pool)
