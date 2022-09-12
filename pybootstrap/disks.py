7c3e524c6a45d8adfd8a9f7831c74102de9b597c
Daniel Baker <daniel.baker@tweag.io>
2022-09-11 18:45:38 -0700

    got one function migrated to disks.py and unit testing working.

--- pybootstrap/disks.py
+++ pybootstrap/disks.py
@@ -19,27 +19,6 @@ class BlockDevice(NamedTuple):
     id: str = ""  # disk-by-id (not from lsblk)
 
 
-def get_block_devices_fields() -> str:
-    # pylint: disable=no-member
-    # pylint: disable=protected-access
-    blk_fields = BlockDevice._fields
-    non_blk_fields = BlockDevice._field_defaults.keys()
-    lsblk_cols = ",".join((key for key in blk_fields if key not in non_blk_fields))
-
-    print(f"lsblk -d --json -o {lsblk_cols}".split())
-    process = subprocess.run(
-        f"lsblk -d --json -o {lsblk_cols}".split(),
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    # print(process.stdout)
-    # block_devices = json.loads(process.stdout)["blockdevices"]
-    # logger.debug(block_devices)
-    # return block_devices
-    return process.stdout
-
-
 def get_block_devices() -> List[BlockDevice]:
     """Creates a list of block devices that are disk types.
 
@@ -52,13 +31,13 @@ def get_block_devices() -> List[BlockDevice]:
     non_blk_fields = BlockDevice._field_defaults.keys()
     lsblk_cols = ",".join((key for key in blk_fields if key not in non_blk_fields))
 
-    process = subprocess.run(
-        f"lsblk -d --json -o {lsblk_cols}".split(),
-        capture_output=True,
-        text=True,
-        check=False,
-    )
-    block_devices = json.loads(process.stdout)["blockdevices"]
+    with subprocess.Popen(
+        f"lsblk -d --json -o {lsblk_cols}".split(), stdout=subprocess.PIPE
+    ) as process:
+        stdout_data, _ = process.communicate()
+        process.terminate()
+
+    block_devices = json.loads(stdout_data.decode("utf-8"))["blockdevices"]
     block_devices = [BlockDevice(**dev) for dev in block_devices]
     disks_only = list(filter(lambda dev: dev.type == "disk", block_devices))
     return disks_only
---
+++ pybootstrap/test/test_disks.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+
+from testfixtures.comparison import compare
+from testfixtures.popen import MockPopen
+from testfixtures.replace import Replacer
+
+from pybootstrap.disks import BlockDevice, get_block_devices
+
+
+class TestBlockDevices(TestCase):
+    def setUp(self):
+        self.Popen = MockPopen()
+        self.r = Replacer()
+        self.r.replace("subprocess.Popen", self.Popen)
+        self.addCleanup(self.r.restore)
+
+    def test_example(self):
+        # set up
+        stdout = b"""
+        {                                                                                                                                                             "blockdevices": [
+                {"name":"loop0", "kname":"loop0", "path":"/dev/loop0", "model":null, "serial":null, "size":"4K", "type":"loop"},
+                {"name":"loop1", "kname":"loop1", "path":"/dev/loop1", "model":null, "serial":null, "size":"113.9M", "type":"loop"},
+                {"name":"hda", "kname":"hda", "path":"/dev/hda", "model":"Toshiba_S300Pro", "serial":"1234567890", "size":"9.9TB", "type":"disk"},
+                {"name":"sda", "kname":"sda", "path":"/dev/sda", "model":"SanDisk_SD9SN8W512G", "serial":"183347423947", "size":"465.8G", "type":"disk"},
+                {"name":"nvme0n1", "kname":"nvme0n1", "path":"/dev/nvme0n1", "model":"Samsung SSD 970 PRO 1TB", "serial":"S462NF0K816201E", "size":"953.9G", "type":"disk"}
+            ]
+        }"""
+        lsblk_cmd = "lsblk -d --json -o name,kname,path,model,serial,size,type"
+        self.Popen.set_command(lsblk_cmd, stdout=stdout, stderr=b"e")
+
+        # testing of results
+        truth = [
+            BlockDevice(
+                name="hda",
+                kname="hda",
+                path="/dev/hda",
+                model="Toshiba_S300Pro",
+                serial="1234567890",
+                size="9.9TB",
+                type="disk",
+                id="",
+            ),
+            BlockDevice(
+                name="sda",
+                kname="sda",
+                path="/dev/sda",
+                model="SanDisk_SD9SN8W512G",
+                serial="183347423947",
+                size="465.8G",
+                type="disk",
+                id="",
+            ),
+            BlockDevice(
+                name="nvme0n1",
+                kname="nvme0n1",
+                path="/dev/nvme0n1",
+                model="Samsung SSD 970 PRO 1TB",
+                serial="S462NF0K816201E",
+                size="953.9G",
+                type="disk",
+                id="",
+            ),
+        ]
+        compare(get_block_devices(), truth)
