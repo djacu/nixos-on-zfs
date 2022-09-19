f1b1488a700a5b29de1264611cbd187e6e554213
Daniel Baker <daniel.baker@tweag.io>
2022-09-18 20:41:53 -0700

    verify that pybootstrap is run as root before anything else.

--- pybootstrap/pybootstrap/bootstrap.py
+++ pybootstrap/pybootstrap/bootstrap.py
@@ -1,7 +1,22 @@
+import os
+
 from pybootstrap import configure, install, partition, prepare
 
 
+def _verify_root():
+    if os.geteuid() != 0:
+        exit(
+            "\n".join(
+                [
+                    "You need to have root privileges to run this script.",
+                    "Please try again, this time using 'sudo'. Exiting.",
+                ]
+            )
+        )
+
+
 def main():
+    _verify_root()
     config = prepare.prepare()
     partition.partition(config=config)
     configure.configure(config=config)
